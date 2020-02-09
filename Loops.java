44df8acbbc1d9109e47d3628495d17ea4c9bc15d
Dave Irwin <david@redirwin.com>
2020-02-08 19:58:11 -0700

    Part 5a completed.

--- Loops.java
+++ Loops.java
@@ -4,6 +4,15 @@
 public class Loops {
 
     public static void main(String[] args) {
+
+        // Start Assignment Part 5a
+
+        int i = 0;
+
+        while (i <= 100) {
+            System.out.print(i + " ");
+            i += 2;
+        }
         
     }
 }
