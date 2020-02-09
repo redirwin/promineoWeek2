5f0726c83863832554c2fbc350691e08a2f495fc
Dave Irwin <david@redirwin.com>
2020-02-08 18:54:54 -0700

    Part 2 & 3 variables created.

--- Week2Assignment.java
+++ Week2Assignment.java
@@ -4,6 +4,20 @@
 public class Week2Assignment {
 
     public static void main(String[] args) {
-        System.out.println("Hello from VS Code!");
+
+        // Start Assignment Part 2
+
+        Boolean isHotOutside = true;
+        Boolean isWeekday = false;
+        Boolean hasMoneyInPocket = false;
+
+        // Start Assignement Part 3
+
+        double costOfMilk = 2.99;
+        double moneyInWallet = 20.00;
+        int thirstLevel = 8;
+
+        System.out.println("I should buy icecream: " + shouldBuyIcecream);
+        
     }
 }
