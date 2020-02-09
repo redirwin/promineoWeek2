a1ffe8e22d28390a689821dff0107c6fde25bb09
Dave Irwin <david@redirwin.com>
2020-02-08 19:05:44 -0700

    Part 4 variables created.

--- Week2Assignment.java
+++ Week2Assignment.java
@@ -9,15 +9,26 @@ public class Week2Assignment {
 
         Boolean isHotOutside = true;
         Boolean isWeekday = false;
-        Boolean hasMoneyInPocket = false;
+        Boolean hasMoneyInPocket = true;
 
         // Start Assignement Part 3
 
         double costOfMilk = 2.99;
-        double moneyInWallet = 20.00;
-        int thirstLevel = 8;
+        double moneyInWallet = 6;
+        int thirstLevel = 3;
+        
+        // Start Assignment Part 4
+
+        Boolean shouldBuyIcecream = isHotOutside && hasMoneyInPocket;
+        Boolean willGoSwimming = isHotOutside && !isWeekday;
+        Boolean isAGoodDay = isHotOutside && hasMoneyInPocket && !isWeekday;
+        Boolean willBuyMilk = isHotOutside && thirstLevel >= 3 && moneyInWallet >= 2 * costOfMilk;
 
         System.out.println("I should buy icecream: " + shouldBuyIcecream);
+        System.out.println("I should go swimming: " + willGoSwimming);
+        System.out.println("It's a good day: " + isAGoodDay);
+        System.out.println("I should buy milk: " + willBuyMilk);
+
         
     }
 }
