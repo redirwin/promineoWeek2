60a986fac53396e28b22925548ae840e4f5db274
Dave Irwin <david@redirwin.com>
2020-02-08 20:03:18 -0700

    Part 5b completed.

--- Loops.java
+++ Loops.java
@@ -5,7 +5,8 @@ public class Loops {
 
     public static void main(String[] args) {
 
-        // Start Assignment Part 5a
+        // Start Assignment Part 5a 
+        // A while loop that prints all even numbers from 0 to 100
 
         int i = 0;
 
@@ -13,6 +14,19 @@ public class Loops {
             System.out.print(i + " ");
             i += 2;
         }
+
+        System.out.println();
+        System.out.println();
         
+        // Start Part 5b
+        // A while loop that prints every 3rd number going backwards from 100 until we reach 0
+
+        int j = 100;
+
+        while (j > 0) {
+            System.out.print(j + " ");
+            j -= 3;
+        }
+
     }
 }
